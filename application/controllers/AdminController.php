611bb4397a67a82d63ad35c28a263dcd98172160
Sudheer P <sudheerp@in4biz.com>
2016-09-22 18:55:20 +0530

    Updated login view and admin dashboard

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        echo 'hai admin';
+        //echo 'hai admin';
     }
 
 	public function registerAction()
