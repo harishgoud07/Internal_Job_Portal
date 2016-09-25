e6babc891a53c0e77d5d4f0515b7dd92230640fe
Sudheer P <p.sudheer747@gmail.com>
2016-09-25 15:31:12 +0530

    Updated jobs view

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,55 +1,42 @@
 <?php
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-	public function preDispatch(Zend_Controller_Request_Abstract $request)
-	{
-		$layout = Zend_Layout::getMvcInstance();
-
-		$view = $layout->getView();
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	public function preDispatch(Zend_Controller_Request_Abstract $request) {
+		$layout = Zend_Layout::getMvcInstance ();
+		
+		$view = $layout->getView ();
 	}
-
-	protected function _initAutoload()
-	{
-		$loader = new Zend_Application_Module_Autoloader(array(
-			'namespace' => '',
-			'basePath'  => APPLICATION_PATH));
-Zend_Loader_Autoloader::getInstance()->registerNamespace('Application_models_');
-
-    
-
+	protected function _initAutoload() {
+		$loader = new Zend_Application_Module_Autoloader ( array (
+				'namespace' => '',
+				'basePath' => APPLICATION_PATH 
+		));
+		Zend_Loader_Autoloader::getInstance ()->registerNamespace ( 'Application_models_' );
+		
 		return $loader;
 	}
-
-	protected function _initSession()
-	{
-		/*$session = new Zend_Session_Namespace('project', true);
-
-		return $session;*/
+	protected function _initSession() {
+		/*
+		 * $session = new Zend_Session_Namespace('project', true);
+		 *
+		 * return $session;
+		 */
 	}
-
-	protected function _initDB()
-	{
-		$config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+	protected function _initDB() {
+		$config = new Zend_Config_Ini ( APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV );
 		$db_params = $config->resources->db->params;
 		
-		$params = array(
-    'host'           => $db_params->host,
-    'username'       => $db_params->username,
-    'password'       => $db_params->password,
-    'dbname'         => $db_params->dbname,
-    
-);
-
-$db = Zend_Db::factory($config->resources->db->adapter, $params);
-		Zend_Db_Table::setDefaultAdapter($db);
+		$params = array (
+				'host' => $db_params->host,
+				'username' => $db_params->username,
+				'password' => $db_params->password,
+				'dbname' => $db_params->dbname 
+		);
+		
+		$db = Zend_Db::factory ( $config->resources->db->adapter, $params );
+		Zend_Db_Table::setDefaultAdapter ( $db );
 	}
-
-
-	protected function _initRegisterPlugins()
-	{
-		$front = Zend_Controller_Front::getInstance();
-       $front->registerPlugin(new application_plugins_session());
+	protected function _initRegisterPlugins() {
+		$front = Zend_Controller_Front::getInstance ();
+		$front->registerPlugin ( new application_plugins_session () );
 	}
-
-	
 }
--- externals/js/dashboard.js
+++ externals/js/dashboard.js
@@ -1,4 +1,10 @@
-$(function () {
+$(document).ready(function(){  
+	$('#details').on('show.bs.collapse', function () {
+		$('.toggle-icon').css({'transform' : 'rotate(180deg)'});
+	});
 	$('expiry_date_datepicker').datetimepicker();
-    $('.chosen-select').chosen();
+	$('.chosen-select').chosen();
+	$('#details').on('hide.bs.collapse', function () {
+		$('.toggle-icon').css({'transform' : 'rotate(0deg)','transform-origin':'center center'});
+	});
 });
