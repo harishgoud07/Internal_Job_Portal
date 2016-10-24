8bb608c793ba7ced9a75593e6c5f1a6a1c8f7e60
Sudheer P <sudheerp@in4biz.com>
2016-10-24 20:40:39 +0530

    added view to show employees

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -20,6 +20,18 @@ class AdminController extends Zend_Controller_Action
         $this->view->projects_list = $projects_list;
         $this->view->active_posts_data = $active_posts_data;
     }
+
+    public function browseemployeesAction() {
+        $storage = Zend_Auth::getInstance()->getIdentity();
+        if($storage) {
+            $storage->active_page = "browse_employees";
+        }
+        $posts = new application_models_Posts();
+        $projects_list = $posts->get_projects_list ();
+        $this->view->projects_list = $projects_list;
+        $this->view->managers_list = array();
+    }
+
     public function loginrequestsAction()
     {
         $this->_forward('index', 'loginrequests', null);
