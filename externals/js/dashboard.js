1c6684312216c9a460d6036f3e111f52b31b085f
Santhosh.T <santhoshthatipelli285@gmail.com>
2016-11-12 01:21:15 +0530

    addedd validations

--- externals/js/dashboard.js
+++ externals/js/dashboard.js
@@ -1,9 +1,13 @@
-$(function(){  
+$(function(){ 
 	$('#details').on('show.bs.collapse', function () {
 		$('.toggle-icon').css({'transform' : 'rotate(180deg)'});
 	});
+	var hour = new Date().getHours();
 	$('.post-job-date-control').datetimepicker({
-			format: 'YYYY-MM-DD HH:mm:ss'
+			
+			format:'YYYY-MM-DD HH:mm:ss',
+			minDate: moment({h:hour+2})
+			
 	});
 	$('#key_skills').chosen();
 	$('#details').on('hide.bs.collapse', function () {
